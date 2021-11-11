bed94fdca15d7b3ea1e65da4c304b48eff9e992e
Wayne Rocha <dev.waynerocha@gmail.com>
2021-11-11 11:58:03 -0300

    <built> ✅funcionando

--- code/code.ino
+++ code/code.ino
@@ -1,7 +1,7 @@
-#define IN1 0 
-#define IN2 0
-#define IN3 0 
-#define IN4 0
+#define IN1 5
+#define IN2 4
+#define IN3 0
+#define IN4 2
 
 #define TCRT5000_A 12
 #define TCRT5000_B 13
@@ -9,24 +9,38 @@
 bool sensorA_state = 0;
 bool sensorB_state = 0;
 
-void setup(){
+void setup() {
   pinMode(TCRT5000_A, INPUT);
   pinMode(TCRT5000_B, INPUT);
-  
+
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
+
   Serial.begin(115200);
 }
- 
+
 void loop() {
   sensorA_state = digitalRead(TCRT5000_A);
   sensorB_state = digitalRead(TCRT5000_B);
-  
-  printSensorsState();
+
+  printSensorStates();
   printMotorStates();
-  
-  delay(2000);
+
+  if (!sensorA_state && !sensorB_state) {
+    stopWheels();
+  } else if (sensorA_state && sensorB_state) {
+    setDirectionToCenter();
+  } else if (!sensorA_state) {
+    setDirectionToLeft();
+  } else if (!sensorB_state) {
+    setDirectionToRight();
+  }
+  delay(50);
 }
 
-void printSensorsState(){
+void printSensorStates() {
   Serial.println();
   Serial.println("-----SENSORES-----");
   Serial.print("sensor A: ");
@@ -34,36 +48,48 @@ void printSensorsState(){
   Serial.print("sensor B: ");
   Serial.println(sensorB_state);
 }
-void printMotorStates(){ 
+void printMotorStates() {
   Serial.println();
   Serial.println("-----MOTORES-----");
-  if (!sensorA_state && !sensorB_state){
+  if (!sensorA_state && !sensorB_state) {
     Serial.println("Motor A: desligado");
     Serial.println("Motor B: desligado");
-  } else if (sensorA_state && sensorB_state){
+  } else if (sensorA_state && sensorB_state) {
     Serial.println("Motor A: ligado");
     Serial.println("Motor B: ligado");
-  } else if (!sensorA_state){
+  } else if (!sensorA_state) {
     Serial.println("Motor A: ligado");
     Serial.println("Motor B: desligado");
-  } else if (!sensorB_state){
+  } else if (!sensorB_state) {
     Serial.println("Motor A: desligado");
     Serial.println("Motor B: ligado");
   }
 }
 
-void setDirectionToLeft(){
-  
+void setDirectionToLeft() {
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, HIGH);
 }
- 
-void setDirectionToRight(){
-  
+
+void setDirectionToRight() {
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, HIGH);
 }
- 
-void setDirectionToCenter(){
-  
+
+void setDirectionToCenter() {
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
 }
- 
-void stopWheels(){
-    
+
+void stopWheels() {
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, HIGH);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, HIGH);
 }
