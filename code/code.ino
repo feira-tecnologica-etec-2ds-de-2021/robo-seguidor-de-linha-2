b4999b1844d4ad36159863a8ac216b0502e9be11
Wayne Rocha <dev.waynerocha@gmail.com>
2021-11-18 22:22:30 -0300

    Update code.ino

--- code/code.ino
+++ code/code.ino
@@ -1,26 +1,24 @@
 #include <ESP8266WiFi.h>
 #include <espnow.h>
 
-#define SEMAPHORE_RED "vermelho"
-#define SEMAPHORE_YELLOW "amarelo"
-#define SEMAPHORE_GREEN "verde"
+#define SEMAPHORE_RED 3
+#define SEMAPHORE_YELLOW 2
+#define SEMAPHORE_GREEN 1
 
 typedef struct struct_message {
-    String state;
+  int state;
 } struct_message;
 
 struct_message semaphore = {
-  .state = SEMAPHORE_RED
+  .state = SEMAPHORE_GREEN
 };
 
 // Callback function that will be executed when data is received
 void OnDataRecv(uint8_t * mac, uint8_t *incomingData, uint8_t len) {
   memcpy(&semaphore, incomingData, sizeof(semaphore));
   Serial.print("Semaforo esta ");
-  Serial.println(semaphore.state);
-  Serial.println();
+  Serial.println((semaphore.state == SEMAPHORE_RED) ? "vermelho" : ((semaphore.state == SEMAPHORE_YELLOW) ? "amarelo" : "verde"));
 }
-
 
 void setup() {
   Serial.begin(115200);
@@ -31,14 +29,18 @@ void setup() {
     Serial.println("Error initializing ESP-NOW");
     return;
   }
-  
+
   // Once ESPNow is successfully Init, we will register for recv CB to
   // get recv packer info
   esp_now_set_self_role(ESP_NOW_ROLE_SLAVE);
   esp_now_register_recv_cb(OnDataRecv);
-  
 }
 
 void loop() {
-  
+
+}
+
+void showMACAddress() {
+  Serial.print("ESP8266 Board MAC Address:  ");
+  Serial.println(WiFi.macAddress());
 }
